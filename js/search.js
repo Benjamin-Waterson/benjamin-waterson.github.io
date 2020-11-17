79f7b512ff8e3f85346fd668fccef63e10263bdf
Ben Waterson <benwaterson@Mac-mini.local>
2020-11-16 23:23:18 -0700

    Misc. Site Updates

--- js/search.js
+++ js/search.js
@@ -1,11 +1,24 @@
-var input = document.getElementById("input");
-input.addEventListener("keydown", function (e) {
-    if (e.keyCode === 13) {
-        validate(e);
-    }
-});
+// var input = document.getElementById("input");
+// input.addEventListener("keydown", function (e) {
+//     if (e.keyCode === 13) {
+//         validate(e);
+//     }
+// });
 
-function validate(e) {
-var input = document.getElementById("input").value;
-location.replace("https://www.google.com/search?q=site:www.benjaminwaterson.com" + " " + input + "");
+// function validate(e) {
+// var input = document.getElementById("input").value;
+// location.replace("https://www.google.com/search?q=site:www.benjaminwaterson.com" + " " + input + "");
+// };
+(function() {   
+    var cx = '!!!!!!!!!!!!!!!!!!!';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=25d8b607b60f0155b'+ cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
+
+window.onload = function(){
+document.getElementById('gsc-i-id1').placeholder = 'SEARCH';
 };
